056247ede6c30d3a3f05f1e3fd586001467233a7
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-09-06 20:39:59 +0100

    Add data service

---
+++ src/app/countries/shared/data.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Country } from './models/country';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private readonly http: HttpClient) { }
+
+  getAllCountries(): Observable<Country[]> {
+    const url = `${environment.baseUrl}/all`;
+    return this.http.get<Country[]>(url);
+  }
+}
