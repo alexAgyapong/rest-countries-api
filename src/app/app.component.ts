d3c88ffe8fda68ebfb7414cecacd8eba53575ab4
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-09-16 20:27:40 +0100

    Toggle theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rest-countries-api';
+  theme: Theme = this.getThemeFromLocalStorage() || 'light-theme';
+
+  constructor(@Inject(DOCUMENT) private readonly document: Document, private readonly renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.renderer.addClass(this.document.body, this.theme);
+  }
+
+  toggleTheme(): void {
+    const newTheme = this.theme === 'light-theme' ? 'dark-theme' : 'light-theme';
+    this.document.body.classList.replace(this.theme, newTheme);
+    this.theme = newTheme;
+    this.addThemeToLocalStorage(this.theme);
+  }
+
+  addThemeToLocalStorage(theme: Theme): void {
+    localStorage.setItem('theme', theme);
+  }
+
+  getThemeFromLocalStorage(): Theme {
+    return localStorage.getItem('theme') as Theme;
+  }
 }
+
+type Theme = 'light-theme' | 'dark-theme';
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  @Output() themeSwitched = new EventEmitter();
 
-  ngOnInit(): void {
+
+  toggleTheme(): void {
+    this.themeSwitched.emit();
   }
-
 }
